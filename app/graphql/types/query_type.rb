0c9430c9e7a2a135f6ce3f17ef5f1e69472524fa
Oli Guei <olivrg@gmail.com>
2020-01-13 12:36:42 +0000

    Add graphql endpoint

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,14 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    # return all items from the library
+    field :items,
+      [Types::ItemType],
+      null: false,
+      description: 'Returns a list of items in the library'
+    # resolver method for the items field - resolver method name must
+    # match field name
+    def items
+      Item.all
     end
   end
 end
