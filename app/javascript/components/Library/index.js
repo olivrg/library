3977b2a6c4fe1cc401d21d00bbfdca6e46d2e369
Oli Guei <olivrg@gmail.com>
2020-01-13 13:11:56 +0000

    Pass client instances to react components

---
+++ app/javascript/components/Library/index.js
@@ -0,0 +1,31 @@
+import gql from "graphql-tag";
+import React from "react";
+import { Query } from "react-apollo";
+
+const LibraryQuery = gql`
+  {
+    items {
+      id
+      title
+      user {
+        email
+      }
+    }
+  }
+`;
+
+export default () => (
+  <Query query={LibraryQuery}>
+    {({ data, loading }) => (
+      <div>
+        {loading
+          ? "loading..."
+          : data.items.map(({ title, id, user }) => (
+              <div key={id}>
+                <b>{title}</b> {user ? `added by ${user.email}` : null}
+              </div>
+            ))}
+      </div>
+    )}
+  </Query>
+);
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,1 +1,11 @@
-console.log("👻");
+import React from "react";
+import { render } from "react-dom";
+import Library from "../components/Library";
+import Provider from "../components/Provider";
+
+render(
+  <Provider>
+    <Library />
+  </Provider>,
+  document.querySelector("#root")
+);
