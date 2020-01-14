a4cc60db971ee88e1b013a7b9733c35977301d65
Oli Guei <olivrg@gmail.com>
2020-01-14 17:21:53 +0000

    Add tokens to apollo client

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import { render } from "react-dom";
 import Library from "../components/Library";
 import Provider from "../components/Provider";
+import UserInfo from "../components/UserInfo";
 
 render(
   <Provider>
+    <UserInfo />
     <Library />
   </Provider>,
   document.querySelector("#root")
