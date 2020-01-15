29fa3c5b91996959869e16c62a15a412f8203938
Oli Guei <olivrg@gmail.com>
2020-01-15 17:23:28 +0000

    Add UpdateItemForm component

--- app/javascript/components/UpdateItemForm/index.js
+++ app/javascript/components/UpdateItemForm/index.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import cs from './styles';
+import React from "react";
+import { Mutation } from "react-apollo";
+import { UpdateItemMutation } from "./operations.graphql";
+import ProcessItemForm from "../ProcessItemForm";
+import cs from "./styles";
 
-const UpdateItemForm = () => (
-  <div className={cs.updateItemForm}>👋</div>
+const UpdateItemForm = ({
+  id,
+  initialTitle,
+  initialDescription,
+  initialImageUrl,
+  onClose
+}) => (
+  <div className={cs.overlay}>
+    <div className={cs.content}>
+      <Mutation mutation={UpdateItemMutation}>
+        {(updateItem, { loading }) => (
+          <ProcessItemForm
+            initialImageUrl={initialImageUrl}
+            initialTitle={initialTitle}
+            initialDescription={initialDescription}
+            buttonText="Update Item"
+            loading={loading}
+            onProcessItem={({ title, description, imageUrl }) => {
+              updateItem({
+                variables: {
+                  id,
+                  title,
+                  description,
+                  imageUrl
+                }
+              });
+              onClose();
+            }}
+          />
+        )}
+      </Mutation>
+      <button className={cs.close} onClick={onClose}>
+        Close
+      </button>
+    </div>
+  </div>
 );
 
 export default UpdateItemForm;
