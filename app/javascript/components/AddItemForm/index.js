ad288d83e11316d091712fb401ba6f4b494e6a40
Oli Guei <olivrg@gmail.com>
2020-01-15 15:09:50 +0000

    Add form page

--- app/javascript/components/AddItemForm/index.js
+++ app/javascript/components/AddItemForm/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import cs from './styles';
+import React from "react";
+import { Mutation } from "react-apollo";
+import ProcessItemForm from "../ProcessItemForm";
+import { AddItemMutation } from "./operations.graphql";
 
 const AddItemForm = () => (
-  <div className={cs.addItemForm}>👋</div>
+  <Mutation mutation={AddItemMutation}>
+    {(addItem, { loading }) => (
+      <ProcessItemForm
+        buttonText="Add Item"
+        loading={loading}
+        onProcessItem={({ title, description, imageUrl }) =>
+          addItem({
+            variables: {
+              title,
+              description,
+              imageUrl
+            }
+          })
+        }
+      />
+    )}
+  </Mutation>
 );
 
 export default AddItemForm;
