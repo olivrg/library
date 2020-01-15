f1db686a294632258fac4137a3b53f3286a241c5
Oli Guei <olivrg@gmail.com>
2020-01-15 10:32:12 +0000

    Add library query operation

--- app/javascript/components/Library/index.js
+++ app/javascript/components/Library/index.js
@@ -1,31 +1,8 @@
-import gql from "graphql-tag";
-import React from "react";
-import { Query } from "react-apollo";
+import React from 'react';
+import cs from './styles';
 
-const LibraryQuery = gql`
-  {
-    items {
-      id
-      title
-      user {
-        email
-      }
-    }
-  }
-`;
-
-export default () => (
-  <Query query={LibraryQuery}>
-    {({ data, loading }) => (
-      <div>
-        {loading
-          ? "loading..."
-          : data.items.map(({ title, id, user }) => (
-              <div key={id}>
-                <b>{title}</b> {user ? `added by ${user.email}` : null}
-              </div>
-            ))}
-      </div>
-    )}
-  </Query>
+const Library = () => (
+  <div className={cs.library}>👋</div>
 );
+
+export default Library;
