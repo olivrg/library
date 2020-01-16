884c1e84e6a84a7b8e3844bf8c3284cefbc5a1dc
Oli Guei <olivrg@gmail.com>
2020-01-16 11:33:21 +0000

    Refactor auth code

--- app/graphql/mutations/update_item_mutation.rb
+++ app/graphql/mutations/update_item_mutation.rb
@@ -5,7 +5,7 @@ module Mutations
     argument :description, String, required: false
     argument :image_url, String, required: false
 
-    field :item, Types::ItemType. null: true
+    field :item, Types::ItemType, null: true
     field :errors, [String], null: true
 
     def resolve(id:, title:, description: nil, image_url: nil)
