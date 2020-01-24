da1a5bd7cd426dddc25a07edc2b3a245fbf6ed28
Oli Guei <olivrg@gmail.com>
2020-01-24 09:24:13 +0000

    Configure the schema to use action cable subscription

--- app/graphql/library_schema.rb
+++ app/graphql/library_schema.rb
@@ -1,4 +1,7 @@
 class LibrarySchema < GraphQL::Schema
+  use GraphQL::Subscription::ActionCableSubscriptions
+
   mutation(Types::MutationType)
   query(Types::QueryType)
+  subscription(Types::SubscriptionType)
 end
