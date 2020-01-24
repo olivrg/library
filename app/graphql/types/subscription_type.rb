bdfd31c0d004fe9d942fb70b09a5454f40f81900
Oli Guei <olivrg@gmail.com>
2020-01-24 09:21:40 +0000

    Add subscription type

---
+++ app/graphql/types/subscription_type.rb
@@ -0,0 +1,8 @@
+module Types
+  class SubscriptionType < GraphQL::Schema::Object
+    field :item_added, Types::ItemType, null: false, description: "An item was added"
+
+    def item_added,
+    end
+  end
+end
