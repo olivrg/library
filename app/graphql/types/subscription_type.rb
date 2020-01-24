37766c7ce00214b6b5907048f537e2be59aabfe1
Oli Guei <olivrg@gmail.com>
2020-01-24 11:17:44 +0000

    Update items

--- app/graphql/types/subscription_type.rb
+++ app/graphql/types/subscription_type.rb
@@ -1,8 +1,11 @@
 module Types
   class SubscriptionType < GraphQL::Schema::Object
     field :item_added, Types::ItemType, null: false, description: "An item was added"
+    field :item_updated, Types::ItemType, null: false, description: "The item was updated"
 
-    def item_added,
+    def item_added;
+    end
+    def item_updated;
     end
   end
 end
