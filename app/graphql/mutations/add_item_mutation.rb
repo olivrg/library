7e1accb44c19c9f26e38cedf1da7d881bec7bbec
Oli Guei <olivrg@gmail.com>
2020-01-24 11:13:50 +0000

    Test argument

--- app/graphql/mutations/add_item_mutation.rb
+++ app/graphql/mutations/add_item_mutation.rb
@@ -1,19 +1,20 @@
 module Mutations
   class AddItemMutation < Mutations::BaseMutation
-    argument :attributes, Types::ItemAttributes, required: true 
+    argument :attributes, Types::ItemAttributes, required: true
 
     field :item, Types::ItemType, null: true
-    field :errors, Types::ValidationErrorsType, null: true 
+    field :errors, [String], null: false
 
     def resolve(attributes:)
       check_authentication!
 
-      item = Item.new(attributes.to_h.merge(user: context[:current_user])) 
+      item = Item.new(attributes.merge(user: context[:current_user]))
 
       if item.save
+        LibrarySchema.subscriptions.trigger("itemAdded", {}, item)
         { item: item }
       else
-        { errors: item.errors }
+        { errors: item.errors.full_messages }
       end
     end
   end
