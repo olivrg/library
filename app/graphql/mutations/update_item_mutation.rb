6a9b1724d29803da25a12c4f4a4f378a52585a22
Oli Guei <olivrg@gmail.com>
2020-01-25 19:30:27 +0000

    Update item mutation

--- app/graphql/mutations/update_item_mutation.rb
+++ app/graphql/mutations/update_item_mutation.rb
@@ -1,22 +1,18 @@
 module Mutations
   class UpdateItemMutation < Mutations::BaseMutation
     argument :id, ID, required: true
-    argument :title, String, required: true
-    argument :description, String, required: false
-    argument :image_url, String, required: false
+    argument :attributes, Types::ItemAttributes, required: true
 
     field :item, Types::ItemType, null: true
-    field :errors, [String], null: true
+    field :errors, [String], null: false
 
-    def resolve(id:, title:, description: nil, image_url: nil)
-      if context[:current_user].nil?
-        raise GraphQL::ExecutionError,
-              "You need to authenticate to perform this action"
-      end
+    def resolve(id:, attributes:)
+      check_authentication!
 
       item = Item.find(id)
 
-      if item.update(title: title, description: description, image_url: image_url)
+      if item.update(attributess.to_h)
+        LibrarySchema.subscriptions.trigger("itemUpdated", {}, item)
         { item: item }
       else
         { errors: item.errors.full_messages }
